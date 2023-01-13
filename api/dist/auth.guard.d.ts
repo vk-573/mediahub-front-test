23051e099f4a9d897457dcecfdd52a1c7c8566e9
VK <valentin.kiryk@canal-plus.com>
2023-01-13 10:16:05 +0100

    remove dist

--- api/dist/auth.guard.d.ts
+++
@@ -1,8 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
-export declare class AuthGuard implements CanActivate {
-    authService: AuthService;
-    constructor(authService: AuthService);
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
-}
--- api/dist/movies.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express';
-import { MoviesService, Movie } from './movies.service';
-export declare class MoviesController {
-    service: MoviesService;
-    constructor(service: MoviesService);
-    search(query?: string, sortBy?: string): Movie[];
-    get(id: string, res: Response): Response<any, Record<string, any>>;
-}
